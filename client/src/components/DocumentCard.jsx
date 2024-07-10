61f80b5a921ea6a8b4d331cdd14cb2e05ad1250c
Rohan Mali <98544765+RohanM-12@users.noreply.github.com>
2024-07-10 12:04:50 +0530

    Update DocumentCard.jsx

--- client/src/components/DocumentCard.jsx
+++ client/src/components/DocumentCard.jsx
@@ -72,7 +72,7 @@ const DocumentCard = ({ docData, fetchData, shared }) => {
         <p
           className={`text-xs drop-shadow-sm text-gray-500 ${
             shared === 1
-              ? "absolute top-2 left-1/2 transform -translate-x-1/2"
+              ? "absolute top-2 left-1/3 transform -translate-x-1/2"
               : "absolute top-2 left-2"
           }`}
         >
